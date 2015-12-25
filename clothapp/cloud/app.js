ef65bb5eac3b137cb9a38d018d4e91c4430fc8d7
SimoneConia <simoneconia@gmail.com>
2015-12-25 18:46:51 +0100

    Add "/users" route for user profiles management.

--- clothapp/cloud/app.js
+++ clothapp/cloud/app.js
@@ -1,17 +1,20 @@
 
 // These two lines are required to initialize Express in Cloud Code.
- express = require('express');
- app = express();
+express = require('express');
+app = express();
+
+// Using some external files to manage routing.
+require('cloud/routes/users')(app);
 
 // Global app configuration section
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'jade');    // Set the template engine
 app.use(express.bodyParser());    // Middleware for reading request body
 
-// This is an example of hooking up a request handler with a specific request
-// path and HTTP verb using the Express routing API.
+// Request type: GET '/'
+// Result: Render website homepage.
 app.get('/', function(req, res) {
-  res.render('index');
+ 	res.render('index');
 });
 
 // Attach the Express app to Cloud Code.
