cc1d2e333ecc773bb47f6ef9ca515676e0bee300
SimoneConia <simoneconia@gmail.com>
2015-12-27 12:45:29 +0100

    Cleanup app.js structure.

--- clothapp/cloud/app.js
+++ clothapp/cloud/app.js
@@ -1,6 +1,6 @@
 // These two lines are required to initialize Express in Cloud Code.
- express = require('express');
- app = express();
+express = require('express');
+app = express();
 
 // Global app configuration section
 app.set('views', 'cloud/views');  // Specify the folder to find templates
@@ -9,39 +9,39 @@ app.use(express.bodyParser());    // Middleware for reading request body
 
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
-app.get('/', function(req, res) {
-  res.render('index');
+app.get('/', function (req, res) {
+    res.render('index');
 });
 
 //crea nuovo profilo
 app.post('/user/signup', function (req, res) {
-    var user=new Parse.User();
+    var user = new Parse.User();
     user.set("username", req.body.username);
     user.set("password", req.body.password);
     user.set("email", req.body.email);
     
     user.signUp(null, {
-        success: function(user) {
+        success: function (user) {
             // Hooray! Let them use the app now.
             res.send('sigup success');
         },
-        error: function(user, error) {
+        error: function (user, error) {
             // Show the error message somewhere and let the user try again.
             alert("Error: " + error.code + " " + error.message);
             res.send('error');
         }
-    });    
+    });
 });
 
 
 //login al profilo
-app.post('/user/login',function(req,res){
-    Parse.User.logIn (req.body.username,req.body.password, {
-        success: function (user)  {
+app.post('/user/login', function (req, res) {
+    Parse.User.logIn(req.body.username, req.body.password, {
+        success: function (user) {
             //fare cose dopo il login di successo.
             res.send('signin success');
         },
-        error: function (user, error)  {
+        error: function (user, error) {
             // Accesso non riuscito. Controllare l'errore per capire perché.
             res.send('error');
         }
@@ -50,16 +50,16 @@ app.post('/user/login',function(req,res){
 
 //richiedi un profilo
 app.get('/user/:username', function (req, res) {
-    var query=new Parse.Query(Parse.User);
-    query.equalTo("username",req.params.username);
+    var query = new Parse.Query(Parse.User);
+    query.equalTo("username", req.params.username);
     query.find({
-    success: function(results) {      
-      res.send(results);
-    },
-    error: function() {
-      res.send("failed");
-    }
-  });
+        success: function (results) {
+            res.send(results);
+        },
+        error: function () {
+            res.send("failed");
+        }
+    });
 });
 
 
