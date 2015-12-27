04f912fe27d4f36177d0edd918ec9c6b38f1ec75
SimoneConia <simoneconia@gmail.com>
2015-12-27 13:51:52 +0100

    Move "/users" requests from app.js to routes/users.js

--- clothapp/cloud/app.js
+++ clothapp/cloud/app.js
@@ -16,58 +16,5 @@ app.get('/', function (req, res) {
     res.render('index');
 });
 
-//crea nuovo profilo
-app.post('/user/signup', function (req, res) {
-    var user = new Parse.User();
-    user.set("username", req.body.username);
-    user.set("password", req.body.password);
-    user.set("email", req.body.email);
-    
-    user.signUp(null, {
-        success: function (user) {
-            // Hooray! Let them use the app now.
-            res.send('sigup success');
-        },
-        error: function (user, error) {
-            // Show the error message somewhere and let the user try again.
-            alert("Error: " + error.code + " " + error.message);
-            res.send('error');
-        }
-    });
-});
-
-
-//login al profilo
-app.post('/user/login', function (req, res) {
-    Parse.User.logIn(req.body.username, req.body.password, {
-        success: function (user) {
-            //fare cose dopo il login di successo.
-            res.send('signin success');
-        },
-        error: function (user, error) {
-            // Accesso non riuscito. Controllare l'errore per capire perché.
-            res.send('error');
-        }
-    });
-});
-
-//richiedi un profilo
-app.get('/user/:username', function (req, res) {
-    var query = new Parse.Query(Parse.User);
-    query.equalTo("username", req.params.username);
-    query.find({
-        success: function (results) {
-            res.send(results);
-        },
-        error: function () {
-            res.send("failed");
-        }
-    });
-});
-
-
-
-
-
 // Attach the Express app to Cloud Code.
 app.listen();
