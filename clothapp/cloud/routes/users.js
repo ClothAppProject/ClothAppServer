02e725edbf46f21b8a07ffa26f5c3e79816b0acb
SimoneConia <simoneconia@gmail.com>
2015-12-27 12:32:02 +0100

    Initial work on users.js and cleanup existing app.js structure

--- clothapp/cloud/routes/users.js
+++ clothapp/cloud/routes/users.js
@@ -1,8 +1,29 @@
 module.exports = function(app) {
 
-	// Request: GET '/users'
+	// Request:	GET '/users'
 	// Result: Test message.
-	app.get("/users", function(req, res) {
+	app.get('/users', function(req, res) {
 		res.send("Welcome to /users.");
+	});
+	
+	// Request: GET 'users/create'
+	// Params: ND
+	// Result: Creates a new user profile.
+	app.post('/users/create', function (req, res) {
+		//da implementare 
+	});
+
+	//richiedi un profilo
+	app.get('/profile/:name', function (req, res) {
+		var query=new Parse.Query("utenti");
+		query.equalTo("name",req.params.nome);
+		query.find({
+		success: function(results) {      
+		  res.send(results);
+		},
+		error: function() {
+		  res.send("failed");
+		}
+	  });
 	});
 }
