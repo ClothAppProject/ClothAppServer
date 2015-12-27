8c3936979463563bdfc01239d49cc772f768a3ce
SimoneConia <simoneconia@gmail.com>
2015-12-27 17:47:42 +0100

    Working on a test web client

--- clothapp/cloud/app.js
+++ clothapp/cloud/app.js
@@ -2,16 +2,16 @@
 express = require('express');
 app = express();
 
+// Global app configuration section
+app.set('views', 'cloud/views');  // Specify the folder to find templates
+app.set('view engine', 'jade');    // Set the template engine
+app.use(express.bodyParser());    // Middleware for reading request body
+
 // Route requests to /users
 require('cloud/routes/users.js')(app);
 
 // Route requests to a test web client
 require('cloud/routes/test.js')(app);
-
-// Global app configuration section
-app.set('views', 'cloud/views');  // Specify the folder to find templates
-app.set('view engine', 'jade');    // Set the template engine
-app.use(express.bodyParser());    // Middleware for reading request body
 
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
--- clothapp/cloud/routes/test.js
+++ clothapp/cloud/routes/test.js
@@ -3,6 +3,27 @@ module.exports = function (app) {
 	// Request:	GET '/users'
 	// Result: Test message.
 	app.get('/test', function (req, res) {
-		res.send("Welcome to /test.");
+		res.render('test-home');
 	});
+    
+    // Request: POST '/users/signup'
+    // Result: Create a new user with the given username, password, email.
+    app.post('/test/signup', function (req, res) {
+        var user = new Parse.User();
+        user.set("username", req.body.username);
+        user.set("password", req.body.password);
+        user.set("email", req.body.email);
+
+        user.signUp(null, {
+            success: function (user) {
+                // Hooray! Let them use the app now.
+                res.render('test-home');
+            },
+            error: function (user, error) {
+                // Show the error message somewhere and let the user try again.
+                alert("Error: " + error.code + " " + error.message);
+                res.send(error);
+            }
+        });
+    });
 };
