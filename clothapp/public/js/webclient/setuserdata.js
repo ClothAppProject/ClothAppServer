ad60ccefb2936e4db2c78c764ac171c7e7744bac
SimoneConia <simoneconia@gmail.com>
2015-12-29 01:05:08 +0100

    "Set User Profile Data" is now working. Needs login though.

--- clothapp/public/js/webclient/setuserdata.js
+++ clothapp/public/js/webclient/setuserdata.js
@@ -5,31 +5,46 @@ try {
 }
 
 // Get user profile data for the user with the given username
-function OnSubmitUserProfileData() {
-    
-    var username = document.updForm.username.value;
-    var email = document.updForm.email.value;
-    var name = document.updForm.firstName.value;
-    var lastname = document.updForm.lastName.value;
-    var date = new Date(document.updForm.date.value);
-    
-    var user = Parse.User.current();
-    
-    if (username != undefined) user.set('username', username);
-    if (email != undefined) user.set('email', email);
-    if (name != undefined) user.set('name', name);
-    if (lastname != undefined) user.set('lastname', lastname);
-    if (date != undefined) user.set('date', date);
-    
-    user.save(null, {
-        success: function (user) {
-            alert('Save success!');
-            window.location.replace('/webclient');
-        }, function (error) {
-            alert('Save failed...');
-            window.location.replace('/webclient');
+function OnSetData() {
+    alert('submitting...');
+    try {
+        var username = document.setForm.username.value;
+        var email = document.setForm.email.value;
+        var name = document.setForm.firstName.value;
+        var lastname = document.setForm.lastName.value;
+        var date = document.setForm.date.value;
+
+        var user = Parse.User.current();
+
+        if (username != '') {
+            user.set('username', username);
         }
-    })
+        if (email != '') {
+            user.set('email', email);
+        }
+        if (name != '') {
+            user.set('name', name);
+        }
+        if (lastname != '') {
+            user.set('lastname', lastname);
+        }
+        if (date != '') {
+            user.set('date', new Date(date));
+        }
+
+        user.save(null, {
+            success: function (user) {
+                // alert('Save success!');
+                window.location.replace('/webclient');
+            },
+            error: function (error) {
+                // alert('Save failed...');
+                window.location.replace('/webclient');
+            }
+        });
+    } catch (err) {
+        alert(err.message);
+    }
     
     return false;
 }
