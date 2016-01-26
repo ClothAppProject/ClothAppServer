20a98453aed8c7afffb2ef89c654080550f63fb0
jacopo1395 <jack1395@hotmail.it>
2016-01-26 23:15:42 +0100

    non mi va di scrivere un commento

--- clothapp/cloud/routes/users.js
+++ clothapp/cloud/routes/users.js
@@ -6,43 +6,7 @@ module.exports = function (app) {
         res.send("Welcome to /users.");
     });
      
-    /*
-    // Request: POST '/users/signup'
-    // Result: Create a new user with the given username, password, email.
-    app.post('/users/signup', function (req, res) {
-        var user = new Parse.User();
-        user.set("username", req.body.username);
-        user.set("password", req.body.password);
-        user.set("email", req.body.email);
- 
-        user.signUp(null, {
-            success: function (user) {
-                // Hooray! Let them use the app now.
-                res.send('sigup success');
-            },
-            error: function (user, error) {
-                // Show the error message somewhere and let the user try again.
-                // alert("Error: " + error.code + " " + error.message);
-                res.send('error');
-            }
-        });
-    });
- 
-    // Request: POST '/users/login'
-    // Result: Log in a user with the given username and password.
-    app.post('/users/login', function (req, res) {
-        Parse.User.logIn(req.body.username, req.body.password, {
-            success: function (user) {
-                //fare cose dopo il login di successo.
-                res.send('signin success');
-            },
-            error: function (user, error) {
-                // Accesso non riuscito. Controllare l'errore per capire perchè.
-                res.send('error');
-            }
-        });
-    });
-    */
+
  
     // Request: GET '/users/:username'
     // Result: Get the user profile of the user with the given username.
@@ -68,9 +32,16 @@ module.exports = function (app) {
         query.equalTo("username", req.params.username);
         query.find({
             success: function (results) {
-                //TODO: informare il client se la foto non è presente o l'utente non esiste
-                var utente= results[0];
-                res.send(utente.get("fotoProfilo"));
+                try{
+                    //console.log("lunghezza: "+results.length);
+                    if(results.length===0) res.send("this username haven't profile photo or doesn't exist");
+                    else{
+                        var utente= results[0];
+                        var foto=utente.get("ProfilePhoto");
+                        res.send(foto);
+                    }
+                }
+                catch(err){res.send("error: "+err.message)}
             },
             error: function () {
                 res.send("failed");
@@ -78,13 +49,7 @@ module.exports = function (app) {
         });
     });
      
-    // Request: GET '/users/:username/profileThumbnail'
-    // Result: Get the user profile thumbnail of the user with the given username.
-  //  app.get('/users/:username/profileThumbnail', function (req, res) {
-        // non serve?
-    //    res.send('Profile Thumbnail: Da implementare...');
-   // });
-     
+
     // Request: GET '/users/:username/gallery'
     // Result: Get the gallery of the user with the given username.
     app.get('/users/:username/gallery', function (req, res) {
@@ -93,8 +58,7 @@ module.exports = function (app) {
         query.equalTo("user", req.params.username);
         query.find({
             success: function (results) {
-                //TODO: informare il client se user non esiste o no foto
-                if(results.lenght=== 0) res.send('no Foto');
+                if(results.length=== 0) res.send("this username haven't profile photo or doesn't exist");
                 else res.send(results);
             },
             error: function () {
@@ -102,6 +66,9 @@ module.exports = function (app) {
             }
         });
     });
+
+    // Request: GET '/users/:username/galleryLike'
+    // Result: Get photos of a user with the given username order by like.
     app.get('/users/:username/gallerylike', function (req, res) {
             var photo = Parse.Object.extend("Photo");
             var query = new Parse.Query(photo);
@@ -109,8 +76,7 @@ module.exports = function (app) {
             query.descending("numeroLike");
             query.find({
                 success: function (results) {
-                    //TODO: informare il client se user non esiste o no foto
-                    if(results.lenght=== 0) res.send('no Foto');
+                    if(results.length=== 0) res.send("this username haven't profile photo or doesn't exist");
                     else res.send(results);
                 },
                 error: function () {
@@ -118,6 +84,7 @@ module.exports = function (app) {
                 }
             });
         });
+
     // Request: GET '/users/:username/followers'
     // Result: Get the users who follow the user with the given username.
     app.get('/users/:username/followers', function (req, res) {
@@ -125,12 +92,16 @@ module.exports = function (app) {
         query.equalTo("username", req.params.username);
         query.find({
             success: function (results) {
-                //TODO: informare il client se user non esiste o se array vuoto
-                var utente= results[0];
                 try{
-                    res.send(utente.get('followers'));
-                }
-                catch(err){res.send("error:"+err.message);}
+                    if(results.length=== 0) res.send("this username doesn't exist");
+                    else{
+                        var utente= results[0];
+                        var followers=utente.get('followers');
+                        console.log("DEBUG "+utente.get('followers'));
+                        if(typeof followers=='undefined') res.send('undefined');
+                        else res.send(utente.get('followers'));
+                    }
+                }catch(err){res.send("error: "+err.message)}
             },
             error: function () {
                 res.send("failed");
@@ -141,14 +112,19 @@ module.exports = function (app) {
     // Request: GET '/users/:username/following'
     // Result: Get the followed users of the user with the given username.
     app.get('/users/:username/following', function (req, res) {
-        var persona = Parse.Object.extend("Persona")
-        var query = new Parse.Query(persona);
+        var query = new Parse.Query(Parse.User);
         query.equalTo("username", req.params.username);
         query.find({
             success: function (results) {
-                //TODO: informare il client se user non esiste o se array vuoto
-                var utente= results[0];
-                res.send(utente.get('following'));
+                try{
+                    if(results.length=== 0) res.send("this username doesn't exist");
+                    else{
+                        var utente= results[0];
+                        var following=utente.get('following');
+                        if(typeof following=='undefined') res.send('undefined');
+                        else res.send(utente.get('following'));
+                    }
+                }catch(err){res.send("error: "+err.message)}
             },
             error: function () {
                 res.send("failed");
@@ -203,6 +179,7 @@ module.exports = function (app) {
                      //TODO: informare client se user non esisteo se array vuoto
                      var utente= results[0];
                      try{
+                        console.log(utente.get('preferiti'));
                         res.send(utente.get('preferiti'));
                      }
                      catch(err){res.send("error:"+err.message);}
@@ -230,6 +207,53 @@ module.exports = function (app) {
                  }
              });
          });
+
+
+    /*
+        // Request: POST '/users/signup'
+        // Result: Create a new user with the given username, password, email.
+        app.post('/users/signup', function (req, res) {
+            var user = new Parse.User();
+            user.set("username", req.body.username);
+            user.set("password", req.body.password);
+            user.set("email", req.body.email);
+
+            user.signUp(null, {
+                success: function (user) {
+                    // Hooray! Let them use the app now.
+                    res.send('sigup success');
+                },
+                error: function (user, error) {
+                    // Show the error message somewhere and let the user try again.
+                    // alert("Error: " + error.code + " " + error.message);
+                    res.send('error');
+                }
+            });
+        });
+
+        // Request: POST '/users/login'
+        // Result: Log in a user with the given username and password.
+        app.post('/users/login', function (req, res) {
+            Parse.User.logIn(req.body.username, req.body.password, {
+                success: function (user) {
+                    //fare cose dopo il login di successo.
+                    res.send('signin success');
+                },
+                error: function (user, error) {
+                    // Accesso non riuscito. Controllare l'errore per capire perchè.
+                    res.send('error');
+                }
+            });
+        });
+        */
+
+        // Request: GET '/users/:username/profileThumbnail'
+            // Result: Get the user profile thumbnail of the user with the given username.
+          //  app.get('/users/:username/profileThumbnail', function (req, res) {
+                // non serve?
+            //    res.send('Profile Thumbnail: Da implementare...');
+           // });
+
 
     // Request: GET '/users/:username/brands'
     // Result: Get the favorite brands of the user with the given username.
