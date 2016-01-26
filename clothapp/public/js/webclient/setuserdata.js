60cf524c63d16763823234a1df87c06889fe4e8f
SimoneConia <simoneconia@gmail.com>
2016-01-26 23:03:49 +0100

    Update setuserprofiledata page to reflect recent changes to the database.

--- clothapp/public/js/webclient/setuserdata.js
+++ clothapp/public/js/webclient/setuserdata.js
@@ -12,11 +12,72 @@ function OnSetData() {
         var email = document.setForm.email.value;
         var name = document.setForm.firstName.value;
         var lastname = document.setForm.lastName.value;
-        var date = document.setForm.date.value;
+        var date = new Date(document.setForm.date.value);
+        var city = document.setForm.city.value;
 
         var user = Parse.User.current();
+        
+        var Persona = Parse.Object.extend("Persona");
+        
+        var query = new Parse.Query(Persona);
+        query.equalTo('username', user.get("username"));
+        query.first({
+            success: function(persona) {
+                // The object was retrieved successfully.
+                alert(persona);
+                
+                if (username != '') {
+                    user.set('username', username);
+                    persona.set('username', username);
+                }
+                if (email != '') {
+                    user.set('email', email);
+                }
+                if (name != '') {
+                    user.set('name', name);
+                }
+                if (lastname != '') {
+                    persona.set('lastname', lastname);
+                }
+                if (date != '') {
+                    persona.set('date', date);
+                }
+                if (city != '') {
+                    persona.set('city', city);
+                }
 
-        if (username != '') {
+                user.save(null, {
+                    success: function (user) {
+                        alert('User: save success!');
+                        
+                        persona.save(null, {
+                            success: function(persona) {
+                                alert('Persona: save success!');
+                                
+                                window.location.replace('/webclient');
+                            },
+                            error: function(error) {
+                                alert(error.message);
+                            }
+                        });
+                        
+                    },
+                    error: function (error) {
+                        alert(error.message);
+                    }
+                });
+                
+            },
+            error: function(error) {
+                // The object was not retrieved successfully.
+                // error is a Parse.Error with an error code and message.
+                alert(error.message);
+            }
+        });
+        
+        
+
+        /*if (username != '') {
             user.set('username', username);
         }
         if (email != '') {
@@ -29,7 +90,7 @@ function OnSetData() {
             user.set('lastname', lastname);
         }
         if (date != '') {
-            user.set('date', new Date(date));
+            user.set('date', date);
         }
 
         user.save(null, {
@@ -41,7 +102,7 @@ function OnSetData() {
                 // alert('Save failed...');
                 window.location.replace('/webclient');
             }
-        });
+        });*/
     } catch (err) {
         alert(err.message);
     }
