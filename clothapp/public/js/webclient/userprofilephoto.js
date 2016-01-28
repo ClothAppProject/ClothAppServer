8c85cadb228428f76e35762133ee6792acfca41f
SimoneConia <simoneconia@gmail.com>
2016-01-28 12:57:28 +0100

    Initial work on user profile photo page. Still doesn't work.

---
+++ clothapp/public/js/webclient/userprofilephoto.js
@@ -0,0 +1,45 @@
+// Parse initialization
+try {
+    Parse.initialize("JdgT5RtwJaaoDRNe9bx5LF3TyVj20RGXMKMv6jQ8", "MttzOIoQIxkHFd6IuCrsk8AG4FeUy0jTP741gtIg");
+} catch (err) {
+    alert(err.message);
+}
+
+// Get user profile data for the user with the given username
+function getUserProfilePhoto() {
+    
+    try {
+        
+        var username = document.userProfilePhotoForm.username.value;
+
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function () {
+            if (xhttp.readyState === 4 && xhttp.status === 200) {
+                
+                try {
+
+                    var gallery = JSON.parse(xhttp.responseText);
+
+                    var div = document.getElementById("gallery");
+
+                    var content = '<div class="row"><div class="col-sm-6 col-sm-push-3" style="width:300px; height:300px; background-size:cover; background-image:url(\'' + gallery[i].photo.url + '\')" ></div></div>';
+
+                    // alert(content);
+
+                    div.innerHTML = content;
+                } catch (e) {
+                    alert(e.message);
+                }
+            }
+        };
+
+        xhttp.open("GET", "http://clothapp.parseapp.com/users/" + username + "/profilePhoto", true);
+        xhttp.send();
+        
+    } catch (e) {
+        alert(e.message);
+    }
+    
+    return false;
+}
